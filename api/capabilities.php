ef8f6a88c28fbe169f9bf66be3da6c75fba5aca8
jamesjnadeau <james.nadeau@gmail.com>
2014-12-03 20:19:18 +0000

    initial package.json, updates to api scaffold

--- api/capabilities.php
+++ api/capabilities.php
@@ -3,8 +3,8 @@
 require $_SERVER['DOCUMENT_ROOT'].'/secret.php';
 require $_SERVER['DOCUMENT_ROOT'].'/twilio/Twilio.php';
  
-$capability = new Services_Twilio_Capability($accountSid, $authToken);
-$capability->allowClientOutgoing($appSid);
+$capability = new Services_Twilio_Capability($GLOBALS['accountSid'], $GLOBALS['authToken']);
+$capability->allowClientOutgoing($GLOBALS['appSid']);
 $capability->allowClientIncoming('jenny');
 $token = $capability->generateToken();
 echo $token;
