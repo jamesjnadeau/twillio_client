ca9c9edb7ac10a4b8badf62f792b14d5cb312d6c
jamesjnadeau <james.nadeau@gmail.com>
2014-12-07 02:53:33 +0000

    custom twilio client ids

--- api/capabilities.php
+++ api/capabilities.php
@@ -2,9 +2,9 @@
 
 require $_SERVER['DOCUMENT_ROOT'].'/secret.php';
 require $_SERVER['DOCUMENT_ROOT'].'/twilio/Twilio.php';
- 
+//file_put_contents('log', print_r($_REQUEST, true));
 $capability = new Services_Twilio_Capability($GLOBALS['accountSid'], $GLOBALS['authToken']);
 $capability->allowClientOutgoing($GLOBALS['appSid']);
-$capability->allowClientIncoming('jenny');
+$capability->allowClientIncoming(str_replace(':', '', $_REQUEST['clientName']));
 $token = $capability->generateToken();
 echo $token;
--- api/voice_request.php
+++ api/voice_request.php
@@ -5,11 +5,12 @@ header('Content-type: text/xml');
 $callerId = "+18023471430";
  
 // put your default Twilio Client name here, for when a phone number isn't given
-$number   = "jenny";
+//$number   = "jenny";
+$number = $_REQUEST['From'];
  
 // get the phone number from the page request parameters, if given
-if (isset($_REQUEST['PhoneNumber'])) {
-    $number = htmlspecialchars($_REQUEST['PhoneNumber']);
+if (isset($_REQUEST['ClientName'])) {
+    $number = htmlspecialchars($_REQUEST['ClientName']);
 }
  
 // wrap the phone number or client name in the appropriate TwiML verb
@@ -19,9 +20,15 @@ if (preg_match("/^[\d\+\-\(\) ]+$/", $number)) {
 } else {
     $numberOrClient = "<Client>" . $number . "</Client>";
 }
+//file_put_contents('log', print_r($_REQUEST, true));
 ?>
  
 <Response>
+	<?/*
+	<Say>Calling <?php echo $number; ?></Say>
+	<Pause length="2"/>
+	<Say>Calling <?php echo $number; ?></Say>
+	*/?>
     <Dial callerId="<?php echo $callerId ?>">
           <?php echo $numberOrClient ?>
     </Dial>
